19307f68b325a71927d9005af9fc34ec78c103bb
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-10-26 15:58:56 +0900

    angular version 최신화

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-zorro!');
+    expect(page.getTitleText()).toEqual('ng-zorro app is running!');
   });
 
   afterEach(async () => {
